766633595976f403073c60a84229378e335ae11f
Pranav Bhangale <pranavbhangale580@gmail.com>
2022-01-30 14:17:25 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require("path");
-const fs = require("path");
+const fs = require("fs");
 const port = 3000
 
 app.get('/', (req, res) => {
