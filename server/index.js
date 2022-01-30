55e1de5f391e7bf93177e95c95a26944b3567ce9
pranav580 <65352773+pranav580@users.noreply.github.com>
2022-01-30 08:10:01 +0530

    server Creation

---
+++ server/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const path = require("path");
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+const DB_PATH = path.resolve("db.json");
+
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+    fs.readFile(DB_PATH, "utf-8", (err, jsonString) => {
+      if (err) return console.log("Error in reading from db");
+      let values = JSON.parse(jsonString);
+      res.status(200).json({
+        totalValues: values.length,
+        values,
+      });
+    });
+  });
+
+  app.post("/", async (req, res) => {
+    fs.readFile(DB_PATH, "utf-8", (err, jsonString) => {
+      if (err) return console.log("Error in reading from db");
+      let body = req.body;
+      let valuesArr = JSON.parse(jsonString);
+      let obj = {
+        temperature: body.temperature,
+        humidity: body.humidity,
+        timestamp: new Date(),
+      };
+      valuesArr.push(obj);
+      fs.writeFile(DB_PATH, JSON.stringify(valuesArr), (err) => {
+        if (err) return console.log("Error in updating db");
+        res.status(200).json({
+          message: "Values saved",
+          value: valuesArr[valuesArr.length - 1],
+        });
+      });
+    });
+  });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
