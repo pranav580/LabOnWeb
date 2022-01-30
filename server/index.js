dd17723ba0724da3ce0440a7dad165258e96e94b
pranav580 <65352773+pranav580@users.noreply.github.com>
2022-01-30 20:16:32 +0530

    updates

--- server/index.js
+++ server/index.js
@@ -8,43 +8,50 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-const DB_PATH = path.resolve("db.json");
+app.post('/', (req,res)=>{
+  // var S= req.body.json_str;
+  console.log(req.body.json_str);
+  var Ss = "Done";
+  res.send(Ss);
+})
 
-app.use(express.json());
+// const DB_PATH = path.resolve("db.json");
 
-app.get("/", async (req,res) => {
-  console.log(res + "hello");
-    fs.readFile(DB_PATH, "utf-8", (err, jsonString) => {
-      if (err) return console.log("Error in reading from db");
-      let values = JSON.parse(jsonString);
-      res.status(200).json({
-        totalValues: values.length,
-        values,
-      });
-    });
-  });
+// app.use(express.json());
 
-  app.post("/", async (req, res) => {
-    console.log(res + "world");
-    fs.readFile(DB_PATH, "utf-8", (err, jsonString) => {
-      if (err) return console.log("Error in reading from db");
-      let body = req.body;
-      let valuesArr = JSON.parse(jsonString);
-      let obj = {
-        temperature: body.Name1,
-        humidity: body.Name2,
-        timestamp: new Date(),
-      };
-      valuesArr.push(obj);
-      fs.writeFile(DB_PATH, JSON.stringify(valuesArr), (err) => {
-        if (err) return console.log("Error in updating db");
-        res.status(200).json({
-          message: "Values saved",
-          value: valuesArr[valuesArr.length - 1],
-        });
-      });
-    });
-  });
+// app.get("/", async (req,res) => {
+//   console.log(res + "hello");
+//     fs.readFile(DB_PATH, "utf-8", (err, jsonString) => {
+//       if (err) return console.log("Error in reading from db");
+//       let values = JSON.parse(jsonString);
+//       res.status(200).json({
+//         totalValues: values.length,
+//         values,
+//       });
+//     });
+//   });
+
+//   app.post("/", async (req, res) => {
+//     console.log(res + "world");
+//     fs.readFile(DB_PATH, "utf-8", (err, jsonString) => {
+//       if (err) return console.log("Error in reading from db");
+//       let body = req.body;
+//       let valuesArr = JSON.parse(jsonString);
+//       let obj = {
+//         temperature: body.Name1,
+//         humidity: body.Name2,
+//         timestamp: new Date(),
+//       };
+//       valuesArr.push(obj);
+//       fs.writeFile(DB_PATH, JSON.stringify(valuesArr), (err) => {
+//         if (err) return console.log("Error in updating db");
+//         res.status(200).json({
+//           message: "Values saved",
+//           value: valuesArr[valuesArr.length - 1],
+//         });
+//       });
+//     });
+//   });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
