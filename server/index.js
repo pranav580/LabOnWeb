a864c1a4743816548ce6c854991df706b6bb4e47
pranav580 <65352773+pranav580@users.noreply.github.com>
2022-01-31 02:56:14 +0530

    updates

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.post('/data', (req,res)=>{
   // var S= req.body;
   // console.log(S);
   console.log("ONLY REQ.body====");
-  console.log(req.params);
+  console.log(req.url);
   var Ss = "Done";
   res.send(Ss);
 })
