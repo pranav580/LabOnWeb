d8e1fd44f7f55148133c3bdf46d4684f257e0d50
pranav580 <65352773+pranav580@users.noreply.github.com>
2022-02-01 14:49:58 +0530

    frontend added

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <form name='form1' method="get" action="req_data">
+      <form name='form1' method="get" action="http://18.218.176.21:5000/req_data">
         <label for="fname">First name:</label> <br/>
         <input type="text" id="fname" name="fname"/><br/>
         <label for="lname">Last name:</label><br/>
--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const path = require("path");
 const fs = require("fs");
 var bodyParser = require('body-parser');
 const { urlencoded } = require('body-parser');
-const port = 3000
+const port = 5000
 
 app.get('/', (req, res) => {
   res.send('Hello World!' );
@@ -20,43 +20,14 @@ app.post('/data',jsonParser, function(req,res){
   res.send(Ss);
 })
 
-// const DB_PATH = path.resolve("db.json");
-
-// app.use(express.json());
-
-// app.get("/", async (req,res) => {
-//   console.log(res + "hello");
-//     fs.readFile(DB_PATH, "utf-8", (err, jsonString) => {
-//       if (err) return console.log("Error in reading from db");
-//       let values = JSON.parse(jsonString);
-//       res.status(200).json({
-//         totalValues: values.length,
-//         values,
-//       });
-//     });
-//   });
-
-//   app.post("/", async (req, res) => {
-//     console.log(res + "world");
-//     fs.readFile(DB_PATH, "utf-8", (err, jsonString) => {
-//       if (err) return console.log("Error in reading from db");
-//       let body = req.body;
-//       let valuesArr = JSON.parse(jsonString);
-//       let obj = {
-//         temperature: body.Name1,
-//         humidity: body.Name2,
-//         timestamp: new Date(),
-//       };
-//       valuesArr.push(obj);
-//       fs.writeFile(DB_PATH, JSON.stringify(valuesArr), (err) => {
-//         if (err) return console.log("Error in updating db");
-//         res.status(200).json({
-//           message: "Values saved",
-//           value: valuesArr[valuesArr.length - 1],
-//         });
-//       });
-//     });
-//   });
+app.get("/req_data", function(req,res){
+  var str={
+      fist_name:req.query.fname,
+      last_name:req.query.lname
+  };
+  console.log(str);
+  res.send(JSON.stringify(str));
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
